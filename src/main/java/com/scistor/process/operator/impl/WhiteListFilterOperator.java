92bc834a7fb74dce59c578707ecdf16a43a40e6b
王盛铧 <wsh@example.com>
2017-12-18 09:33:18 +0800

    修改输出到hdfs文件大小

--- src/main/java/com/scistor/process/operator/impl/WhiteListFilterOperator.java
+++ src/main/java/com/scistor/process/operator/impl/WhiteListFilterOperator.java
@@ -46,7 +46,7 @@ public class WhiteListFilterOperator implements TransformInterface {
     private static final String KEY1 = "COUNT";
     private static final String KEY2 = "STATUS";
     private static final String PATH = "/data";
-    private static final Integer BATCH_SIZE = 1000;
+    private static final Integer BATCH_SIZE = 200000;
     private Integer index = 1;
     private String zookeeper_addr;
     private String topic = "com.scistor.process.operator.impl.WhiteListFilterOperator";
@@ -206,11 +206,20 @@ public class WhiteListFilterOperator implements TransformInterface {
                 }
                 //进行批量操作，节省资源
                 if (index >= BATCH_SIZE) {
+                    long start = System.currentTimeMillis();
+                    long start1 = System.currentTimeMillis();
                     updateRedis();
+                    long end1 = System.currentTimeMillis();
                     hostCount.clear();
+                    long start2 = System.currentTimeMillis();
                     writeToHDFS();
+                    long end2 = System.currentTimeMillis();
                     messages.clear();
                     index = 0;
+                    long end = System.currentTimeMillis();
+                    LOG.info(String.format("updateRedis consume time:[%s]s", (end1 - start1)/1000));
+                    LOG.info(String.format("writeToHDFS consume time:[%s]s", (end2 - start2)/1000));
+                    LOG.info(String.format("total consume time:[%s]s", (end - start)/1000));
                 }
                 index++;
                 LOG.debug(String.format("已经在Kafka topic:[%s], 消费一条数据:[%s]", topic, message));
